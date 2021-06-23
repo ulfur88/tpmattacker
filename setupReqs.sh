cd213725dc2281f63338e35cb7c352e547147c85
ulfur <s165540@student.dtu.dk>
2021-06-23 14:57:47 +0200

    update setup

--- setupReqs.sh
+++ setupReqs.sh
@@ -3,18 +3,19 @@
 #This script will install all required packages and libraries required to run tpmattacker.py
 
 #Installs Dislocker - used for extracting metadata from the encrypted drive
-sudo apt install dislocker
-
-#Python3 is required to run tpmattacker.py
-sudo apt install python3
+sudo apt -y install dislocker
 
 #Pip3 is used to get the required python libraries
-sudo apt install pip3
+sudo apt -y install python3-pip
 
 #build essential
-sudo apt install build-essential
+sudo apt -y install build-essential
 
-sudo apt install libbde-utils
+sudo apt -y install libbde-utils
+
+sudo apt -y install yosys
+
+sudo apt -y install build-essential clang bison flex libreadline-dev gawk tcl-dev libffi-dev git mercurial graphviz xdot pkg-config python python3 libftdi-dev qt5-default python3-dev libboost-all-dev cmake libeigen3-dev
 
 #python librarires installed
 pip3 install pylibftdi
@@ -22,3 +23,7 @@ pip3 install pylibftdi
 pip3 install sty
 
 pip3 install pycryptodome
+
+
+sudo mkdir /mnt/bitlocker
+sudo mkdir /mnt/ntfs
